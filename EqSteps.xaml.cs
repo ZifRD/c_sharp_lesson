7f5247e10ed7b19e951a452989aec33bf005ab24
Ivan <62903628+ZifRD@users.noreply.github.com>
2020-09-29 01:07:22 +0300

    Add files via upload

---
+++ EqSteps.xaml.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace WindowsFormsApplication6form
+{
+    /// <summary>
+    /// Логика взаимодействия для EqSteps.xaml
+    /// </summary>
+    public partial class EqSteps : UserControl
+    {
+      bool captured = false;
+        double x_shape, x_canvas, y_shape, y_canvas;
+        UIElement source = null;
+        public EqSteps()
+        {
+            InitializeComponent();
+        }
+        private void shape_MouseLeftButtonDown(object sender, MouseButtonEventArgs e)
+        {
+            source = (UIElement)sender;
+            Mouse.Capture(source);
+            captured = true;
+            x_shape = Canvas.GetLeft(source);
+            x_canvas = e.GetPosition(LayoutRoot).X;
+            y_shape = Canvas.GetTop(source);
+            y_canvas = e.GetPosition(LayoutRoot).Y;
+        }
+
+        private void shape_MouseMove(object sender, MouseEventArgs e)
+        {
+            if (captured)
+            {
+                double x = e.GetPosition(LayoutRoot).X;
+                double y = e.GetPosition(LayoutRoot).Y;
+                x_shape += x - x_canvas;
+                Canvas.SetLeft(source, x_shape);
+                x_canvas = x;
+                y_shape += y - y_canvas;
+                Canvas.SetTop(source, y_shape);
+                y_canvas = y;
+            }
+        }
+
+        private void shape_MouseLeftButtonUp(object sender, MouseButtonEventArgs e)
+        {
+            Mouse.Capture(null);
+            captured = false;
+        }
+    }
+}
---
+++ Map.xaml.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Media.Media3D;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace WindowsFormsApplication6form
+{
+    /// <summary>
+    /// Логика взаимодействия для Map.xaml
+    /// </summary>
+    public partial class Map : UserControl
+    {
+        private int picIndex = 0;
+
+        public Map()
+        {
+            InitializeComponent();
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            ImageSourceConverter converter = new ImageSourceConverter();
+            string path = "Resources/";
+
+            switch(picIndex++)
+            {
+                case 0:
+                {
+                    path = "pack://application:,,,/WindowsFormsApplication6form;component/Resources/city12.png";
+                    break;
+                }
+                case 1:
+                {
+                    path = "pack://application:,,,/WindowsFormsApplication6form;component/Resources/city2.png";
+                    break;
+                }
+                case 2:
+                {
+                    path = "pack://application:,,,/WindowsFormsApplication6form;component/Resources/city21.png";
+                    break;
+                }   
+                case 3:
+                {
+                    path = "pack://application:,,,/WindowsFormsApplication6form;component/Resources/city3.png";
+                    break;
+                }                
+            }
+
+            if (picIndex > 4)
+            {
+                picIndex = 0;
+                path = "pack://application:,,,/WindowsFormsApplication6form;component/Resources/city1.png";
+            }
+            ImageSource imageSource = (ImageSource)converter.ConvertFromString(path);
+            brush.ImageSource = imageSource;
+            
+        }
+
+        private void slider_ValueChanged(object sender, RoutedPropertyChangedEventArgs<double> e)
+        {
+           // y is up
+            double xVal = 5.0 - 5.0 * slider.Value / 20.0;
+
+            camera.Position = new Point3D(xVal, 2.2, 0);
+            camera.LookDirection = new Vector3D(-xVal-1E-10, -2.2,0);
+
+        }
+    }
+}
